6072c9d1286796344ebe0376cd3e24bccac73ce8
Pablo Kcres <pablocaceres441@gmail.com>
2024-10-05 01:44:26 -0300

    termine el tp gift

---
+++ giphyApp/giphyApp/src/App.tsx
@@ -0,0 +1,14 @@
+import { GiftList } from "./components/GiftList/GiftList"
+import { NavBar } from "./components/ui/Navbar/Navbar"
+
+function App() {
+  
+  return(
+    <div> 
+    <NavBar   />
+    <GiftList />
+    </div>
+  );
+}
+
+export default App
